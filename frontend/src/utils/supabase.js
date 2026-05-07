5947d525bcf5cda3f07e0394baceac898d71e66c
Jaiprakash Hospitals <hospitalsjaiprakash@gmail.com>
2026-05-07 17:00:13 +0530

    feat:remove not use file and funtion

--- frontend/src/utils/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://focsyzmswqgwiyynjtig.supabase.co';
-const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'sb_publishable_CU29C3-FGatl6RXnOzTxZw_CV-CqAwv';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
