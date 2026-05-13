6e2109aa5ea70cb6d83f3396c3e4f8409565d36e
Jaiprakash Hospitals <hospitalsjaiprakash@gmail.com>
2026-05-13 16:20:36 +0530

    feat:i have implement pwa

---
+++ frontend/public/sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'jphrc-dms-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo512.png',
+  '/favicon.ico'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event (Network First Strategy for stability)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
